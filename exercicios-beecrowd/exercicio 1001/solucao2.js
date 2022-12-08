15770f49c2e4c3e9bea9a06ff04555387de09c59
Jonas Silva <jonas_profijonas_profissional@outlook.com>
2022-12-08 16:26:18 -0300

    solucao 2 exercicio 1001

---
+++ exercicios-beecrowd/exercicio 1001/solucao2.js
@@ -0,0 +1,17 @@
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+
+
+
+
+function calculaAB(A, B){
+    let calculo = A+B;
+    console.log("X = " + calculo);
+}
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+    
+
+calculaAB(A, B);
