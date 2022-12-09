f9b2f3f618ed4277f0ca45dbbd31e841837197b0
Jonas Silva <jonas_profijonas_profissional@outlook.com>
2022-12-09 02:56:26 -0300

    solucao 2

---
+++ exercicios-beecrowd/exercicio 1004/solucao2.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+
+calc(A, B);
+
+function calc(A, B){
+    console.log(`PROD = ${A * B}`);
+}
