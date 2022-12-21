ca2fd3fe355e84884a608b769aab384304f80b0d
Jonas Silva <90778653+jonas-amilton@users.noreply.github.com>
2022-12-21 04:35:08 -0300

    Add files via upload

---
+++ exercicios-beecrowd/exercicio 1049/solucao1.js
@@ -0,0 +1,42 @@
+<script>
+/*
+Neste problema, você deverá ler 3 palavras que 
+definem o tipo de animal
+ possível segundo o esquema abaixo, 
+ da esquerda para a direita.
+Em seguida conclua qual dos animais 
+seguintes foi escolhido, através das três palavras
+ fornecidas.
+ */
+
+ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const data = {
+  vertebrado: {
+    ave: {
+      carnivoro: "aguia",
+      onivoro: "pomba",
+    },
+    mamifero: {
+      onivoro: "homem",
+      herbivoro: "vaca",
+    },
+  },
+  invertebrado: {
+    inseto: {
+      hematofago: "pulga",
+      herbivoro: "lagarta",
+    },
+    anelideo: {
+      hematofago: "sanguessuga",
+      onivoro: "minhoca",
+    },
+  },
+};
+
+const atributos = lines;
+
+console.log(data[atributos[0]][atributos[1]][atributos[2]]);
+
+</script>
