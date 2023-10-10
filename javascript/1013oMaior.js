541fc7c28ee7822fea11a17c14e716032d372124
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-10 11:08:19 -0300

    [update] nova resolução ex.1013

--- javascript/1013oMaior.js
+++ javascript/1013oMaior.js
@@ -1,9 +1,31 @@
 var input = require("fs").readFileSync("/dev/stdin", "utf8");
 var lines = input.split("\n");
 
-const maior = (a, b) => (a + b + Math.abs(a - b)) / 2;
+// const maior = (a, b) => (a + b + Math.abs(a - b)) / 2;
 
-var [a, b, c] = lines.shift().split(" ").map((x) => parseInt(x));
+// var [a, b, c] = lines.shift().split(" ").map((x) => parseInt(x));
+
+// var resposta = maior(a, maior(b, c));
+
+// console.log(resposta + " eh o maior");
+
+
+//abaixo uma segunda resolução
+let [a, b, c] = lines
+  .shift()
+  .split(" ")
+  .map((n) => Number(n));
+
+
+  function maior(a, b) {
+    if (a >= b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  
+
 
 var resposta = maior(a, maior(b, c));
 
