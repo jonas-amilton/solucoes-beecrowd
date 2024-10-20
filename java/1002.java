4b834ad572f3465d70daf66f447e7bc076028633
Jonas Silva <jonasgenro@gmail.com>
2024-10-20 20:23:51 -0300

    Create 1002.java

---
+++ java/1002.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+     Scanner sc = new Scanner(System.in);
+     
+     double n = 3.14159;
+     double r = sc.nextDouble();
+     
+     double a = n * (r*r);
+     
+     System.out.printf("A=%.4f\n", a);
+    }
+ 
+}
